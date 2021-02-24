6cdbefbf93ad252924265e3a157c86b7538be5ef
JiabinYang <360788950@qq.com>
2021-02-24 10:17:47 +0000

    make util compile unit

--- paddle/fluid/extension/include/utils.h
+++ paddle/fluid/extension/include/utils.h
@@ -1,7 +1,21 @@
+// Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #pragma once
 
 #include "paddle/fluid/extension/include/tensor.h"
-namespace paddle{
+namespace paddle {
 
 /////////////////////// Op register API /////////////////////////
 
@@ -14,12 +28,15 @@ extern void RegisterAllCustomOperator();
 // Operator into it
 extern void LoadCustomOperatorLib(const std::string& dso_name);
 
+}  // namespace paddle
+
 #ifdef PADDLE_WITH_CUDA
 #include <cuda_runtime.h>
 
-namespace paddle{
-  extern cudaStream_t GetCurrentStream(const paddle::PlaceType& place);
-}
-#endif
+namespace paddle {
 
-}
+extern cudaStream_t GetCurrentStream(const paddle::PlaceType& place);
+
+}  // namespace paddle
+
+#endif
