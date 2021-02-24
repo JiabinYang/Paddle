d6f56fd1eed73c75c8d912d8b0d747ecda707918
JiabinYang <360788950@qq.com>
2021-02-24 10:18:59 +0000

    make util compile unit

--- paddle/fluid/extension/src/utils.cc
+++ paddle/fluid/extension/src/utils.cc
@@ -1,33 +1,46 @@
+// Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <unordered_map>
+#include "paddle/fluid/extension/include/op_meta_info.h"
 #include "paddle/fluid/framework/custom_operator.h"
 #include "paddle/fluid/framework/custom_tensor_utils.h"
-#include "paddle/fluid/extension/include/op_meta_info.h"
 #ifdef PADDLE_WITH_CUDA
-#include "paddle/fluid/platform/device_context.h"
 #include "paddle/fluid/extension/include/utils.h"
+#include "paddle/fluid/platform/device_context.h"
 #endif
 
 namespace paddle {
 /////////////////////// Op register API /////////////////////////
 
-
 void RegisterAllCustomOperator() {
-    auto& op_meta_info_map = OpMetaInfoMap::Instance();
-    framework::RegisterOperatorWithMetaInfoMap(op_meta_info_map);
-  }
+  auto& op_meta_info_map = OpMetaInfoMap::Instance();
+  framework::RegisterOperatorWithMetaInfoMap(op_meta_info_map);
+}
 
-  void LoadCustomOperatorLib(const std::string& dso_name) {
-    paddle::framework::LoadOpMetaInfoAndRegisterOp(dso_name);
-  }
+void LoadCustomOperatorLib(const std::string& dso_name) {
+  paddle::framework::LoadOpMetaInfoAndRegisterOp(dso_name);
+}
 
 /////////////////////// Op Get Stream API /////////////////////////
 #ifdef PADDLE_WITH_CUDA
-  cudaStream_t GetCurrentStream(const paddle::PlaceType& place){
-    platform::Place inner_place = paddle::framework::CustomOpUtils::ConvertEnumPlaceToInnerPlace(place);
-    platform::DeviceContextPool& pool = platform::DeviceContextPool::Instance();
-    auto* dev_ctx = pool.Get(inner_place);
-    return dynamic_cast<platform::CUDADeviceContext *>(dev_ctx)->stream();
-  }
-#endif
-
+cudaStream_t GetCurrentStream(const paddle::PlaceType& place) {
+  platform::Place inner_place =
+      paddle::framework::CustomOpUtils::ConvertEnumPlaceToInnerPlace(place);
+  platform::DeviceContextPool& pool = platform::DeviceContextPool::Instance();
+  auto* dev_ctx = pool.Get(inner_place);
+  return dynamic_cast<platform::CUDADeviceContext*>(dev_ctx)->stream();
 }
+#endif
+}  // namespace paddle
