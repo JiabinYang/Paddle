df26ff795351a68b6d583b2636b2f13e3bc522a4
JiabinYang <360788950@qq.com>
2021-02-25 02:39:18 +0000

    make util compile unit

--- paddle/fluid/extension/include/utils.h
+++ paddle/fluid/extension/include/utils.h
@@ -15,28 +15,26 @@
 #pragma once
 
 #include "paddle/fluid/extension/include/tensor.h"
+
+#ifdef PADDLE_WITH_CUDA
+#include <cuda_runtime.h>
+#endif
+
 namespace paddle {
 
 /////////////////////// Op register API /////////////////////////
 
 // For inference: compile directly with framework
 // Call after PD_BUILD_OP(...)
-extern void RegisterAllCustomOperator();
+void RegisterAllCustomOperator();
 
 // Using this api to load compiled custom operator's dynamic library and
 // register Custom
 // Operator into it
-extern void LoadCustomOperatorLib(const std::string& dso_name);
-
-}  // namespace paddle
+void LoadCustomOperatorLib(const std::string& dso_name);
 
 #ifdef PADDLE_WITH_CUDA
-#include <cuda_runtime.h>
-
-namespace paddle {
-
-extern cudaStream_t GetCurrentStream(const paddle::PlaceType& place);
+cudaStream_t GetCurrentStream(const PlaceType& place);
+#endif
 
 }  // namespace paddle
-
-#endif
